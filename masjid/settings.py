d09ee5fc44ea6aa5853bb3ce0883ed1dbf4aad8a
Noor Alqoj <noor.alqoj@kuwaitnet.com>
2025-05-02 21:20:26 +0300

    update ALLOWED_HOSTS

--- masjid/settings.py
+++ masjid/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-yu%cbgouyyuxhcaa#das0f*q574ux@sez8&$-4m*2m1f9+#@pf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","masjid.pythonanywhere.com"]
 
 
 # Application definition
